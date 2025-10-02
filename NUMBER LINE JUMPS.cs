7e509b61a2f6dbb16e5d60d2bb787fccbc78bedb
tatojastine-n <jajanicoletato@gmail.com>
2025-10-02 22:06:39 +0800

    NUMBER LINE JUMPS.cs

--- NUMBER LINE JUMPS.cs
+++ NUMBER LINE JUMPS.cs
@@ -1,87 +1,75 @@
-#include <bits/stdc++.h>
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-/*
- * Complete the 'kangaroo' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts following parameters:
- *  1. INTEGER x1
- *  2. INTEGER v1
- *  3. INTEGER x2
- *  4. INTEGER v2
- */
-
-string kangaroo(int x1, int v1, int x2, int v2) {
-
-}
-
-int main()
+class Result
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
 
-    string first_multiple_input_temp;
-    getline(cin, first_multiple_input_temp);
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     */
 
-    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
-
-    int x1 = stoi(first_multiple_input[0]);
-
-    int v1 = stoi(first_multiple_input[1]);
-
-    int x2 = stoi(first_multiple_input[2]);
-
-    int v2 = stoi(first_multiple_input[3]);
-
-    string result = kangaroo(x1, v1, x2, v2);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
+    public static string kangaroo(int x1, int v1, int x2, int v2)
+    {
+        if (v1 == v2)
+        {
+            return (x1 == x2) ? "YES" : "NO";
+        }
+        int numerator = x2 - x1;
+        int denominator = v1 - v2;
+        if (numerator % denominator == 0)
+        {
+            int t = numerator / denominator;
+            if (t >= 0)
+            {
+                return "YES";
+            }
+        }
+        return "NO";
     }
 
-    tokens.push_back(str.substr(start));
+}
 
-    return tokens;
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int x1 = Convert.ToInt32(firstMultipleInput[0]);
+
+        int v1 = Convert.ToInt32(firstMultipleInput[1]);
+
+        int x2 = Convert.ToInt32(firstMultipleInput[2]);
+
+        int v2 = Convert.ToInt32(firstMultipleInput[3]);
+
+        string result = Result.kangaroo(x1, v1, x2, v2);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
 }
